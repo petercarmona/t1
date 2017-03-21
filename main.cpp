9b4a68ad1eb1a50632a6bf4810d99ce5dce9c805
petercarmona <carmonismo@hotmail.com>
2017-03-21 10:31:05 -0500

    Trabajo concluido
    suma, resta, division y multiplicacion de variables

--- main.cpp
+++ main.cpp
@@ -7,13 +7,39 @@ using namespace std;
 
 int main()
 {
-    int a = 4;
-    int b = 5;
+    float a;
+    float b;
+    float c;
 
-    cout<< "La suma de a + b es = " << b + a <<endl;
-    cout<< "la resta de a - b es = " << b - a <<endl;
-    cout<< "la multiplicacion de a * b es = " << b * a <<endl;
-    cout<< "la division de a / b es = " << b / a <<endl;
+
+    cout << "------SUMA-------"<<endl<<endl;
+    cout << "Ingrese el valor de a :";
+    cin >> a;
+    cout << "Ingrese el valor de b :";
+    cin >> b;
+    c= a + b;
+    cout << "La suma de " << a << " + " << b << " es = " << c <<endl<<endl;
+    cout << "------RESTA-------"<<endl<<endl;
+    cout << "Ingrese el valor de a :";
+    cin >> a;
+    cout << "Ingrese el valor de b :";
+    cin >> b;
+    c = a - b;
+    cout << "La resta de " << a << " - " << b << " es = " << c <<endl<<endl;
+    cout << "------MULTIPLICACION-------"<<endl<<endl;
+    cout << "Ingrese el valor de a :";
+    cin >> a;
+    cout << "Ingrese el valor de b :";
+    cin >> b;
+    c = a * b;
+    cout << "La multiplicacion de " << a << " * " << b << " es = " << c <<endl<<endl;
+    cout << "------DIVISION-------"<<endl<<endl;
+    cout << "Ingrese el valor de a :";
+    cin >> a;
+    cout << "Ingrese el valor de b :";
+    cin >> b;
+    c = a / b;
+    cout << "La division de " << a << " / " << b << " es = " << c <<endl;
 
     _getch();
     return 0;
