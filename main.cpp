dd4d57d27235d906d1253dbb677cee094a4b80e5
petercarmona <carmonismo@hotmail.com>
2017-03-21 09:57:04 -0500

    multiplicacins
    cambios de sumas y restas

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 5;
+
+    cout<< "La suma de a + b es = " << b + a <<endl;
+    cout<< "la resta de a - b es = " << b - a <<endl;
+    cout<< "la multiplicacion de a * b es = " << b * a <<endl;
+    cout<< "la division de a / b es = " << b / a <<endl;
+
+    _getch();
+    return 0;
+}
